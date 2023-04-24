6aa1e621321fb4f26560ad000789f9c2edecb582
José Luís <peressousa77@gmail.com>
2023-04-24 02:13:59 -0300

    initial

---
+++ server/index.js
@@ -0,0 +1,108 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+  cors: { origin: "http://localhost:5173" },
+});
+
+app.get("/", (req, res) => {
+  return res.json("Hello, World!");
+});
+
+const port = process.env.PORT || 3001;
+let xIsNext = true;
+
+function next() {
+  if (xIsNext) {
+    return "X";
+  } else {
+    return "O";
+  }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+
+    if (
+      squares[a] &&
+      squares[a].value === squares[b].value &&
+      squares[a].value === squares[c].value
+    ) {
+      return squares[a].value;
+    }
+  }
+
+  return null;
+}
+
+function draw(squares) {
+  if (
+    !calculateWinner(squares) &&
+    squares.every((element) => element.isActive)
+  ) {
+    return true;
+  }
+}
+
+io.on("connection", (socket) => {
+  socket.on("set_channel", (room) => {
+    let players = [];
+    socket.join(room);
+    if (io.sockets.adapter.rooms.get(room).size <= 2) {
+      io.sockets.adapter.rooms.get(room).forEach((element) => {
+        players = [...players, element];
+      });
+      io.to(players[0]).emit("set_side", "X");
+      io.to(players[1]).emit("set_side", "O");
+      io.to(room).emit("room", room);
+      socket.on("set_played", (lugar, squares) => {
+        if (players[0] == socket.id && next() == "X") {
+          squares[lugar].value = "X";
+          squares[lugar].isActive = true;
+          xIsNext = false;
+          if (draw(squares)) {
+            io.to(room).emit("draw");
+            io.to(room).emit("receive_play", squares);
+          } else if (calculateWinner(squares)) {
+            io.to(room).emit("winner", "X");
+            io.to(room).emit("receive_play", squares);
+          } else {
+            io.to(room).emit("next_player", "O");
+            io.to(room).emit("receive_play", squares);
+          }
+        } else if (players[1] == socket.id && next() == "O") {
+          squares[lugar].value = "O";
+          squares[lugar].isActive = true;
+          xIsNext = true;
+          if (draw(squares)) {
+            io.to(room).emit("draw");
+            io.to(room).emit("receive_play", squares);
+          } else if (calculateWinner(squares)) {
+            io.to(room).emit("winner", "O");
+            io.to(room).emit("receive_play", squares);
+          } else {
+            io.to(room).emit("next_player", "X");
+            io.to(room).emit("receive_play", squares);
+          }
+        }
+      });
+    } else if (io.sockets.adapter.rooms.get(room).size > 2) {
+      io.to(socket.id).emit("lot", room);
+    }
+  });
+});
+
+server.listen(port, () => {
+  console.log("Server starting...");
+});

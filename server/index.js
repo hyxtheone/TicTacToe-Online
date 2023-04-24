962ca055ce0bebbf631dd63aa1ca8ca958e1baba
José Luís <peressousa77@gmail.com>
2023-04-24 19:54:00 -0300

    updated to vercel

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const app = require("express")();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server, {
-  cors: { origin: "http://localhost:5173" },
-});
+const io = require("socket.io")(server)
 
 app.get("/", (req, res) => {
   return res.json("Hello, World!");
